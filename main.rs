af5a22f13fa6c723628d30258a564627448f69a7
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-10 19:39:55 +0100

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,18 @@
+pub struct PLC {
+    
+    asyncReg : AsyncRegister;
+    exitHndlr: ExitHandler;
+    eventLoop : ;
+    cfg : Config;
+}
+
+impl PLC {
+    
+}
+
+
+fn event_loop() -> {
+
+
+
+}
